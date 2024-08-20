f2be15455d52319f21e1a35d807c42d1550f1747
ThiagoArzubi <168198724+ThiagoArzubi@users.noreply.github.com>
2024-08-20 14:56:58 -0300

    Agenda_De_Pedidos.py

--- 5°2° Programacion/Agenda_De_Pedidos.py
+++ 5°2° Programacion/Agenda_De_Pedidos.py
@@ -1,27 +1,33 @@
 import os
 os.system("cls")
 
-
+chango={
+    "Papas":2999.99,
+    "VelasX10":1999.99,
+    "Vino":1999.99,
+    "Fernet 1L":9999.99,
+    "Bondiola x kg":6200.00,
+    "Fideos Tallarines 500g":2300.00,
+    "Copos de maiz azucarados 490g":3535.00,
+}
 
 
 #Preguntamos si el que desea ingresar es el propietario o un cliente
 
 
 
-
-# Inicializamos la lista de pedidos
 pedidos = []
 
-# Función para agregar un nuevo pedido
 def agregar_pedido():
     cliente = input("Ingrese el nombre del cliente: ")
-    producto = input("Ingrese el nombre del producto: ")
+    stock()
+    
+    producto = input("Ingrese el numero del producto: ")
     cantidad = int(input("Ingrese la cantidad: "))
     pedido = {"cliente": cliente, "producto": producto, "cantidad": cantidad}
     pedidos.append(pedido)
     print("Pedido agregado con éxito.")
 
-# Función para eliminar un pedido
 def eliminar_pedido():
     cliente = input("Ingrese el nombre del cliente del pedido que desea eliminar: ")
     for pedido in pedidos:
@@ -31,7 +37,6 @@ def eliminar_pedido():
             return
     print("Pedido no encontrado.")
 
-# Función para mostrar todos los pedidos
 def mostrar_pedidos():
     if not pedidos:
         print("No hay pedidos en la agenda.")
@@ -39,7 +44,6 @@ def mostrar_pedidos():
         for idx, pedido in enumerate(pedidos, start=1):
             print(f"Pedido {idx}: Cliente: {pedido['cliente']}, Producto: {pedido['producto']}, Cantidad: {pedido['cantidad']}")
 
-# Función principal que muestra el menú y maneja la interacción del usuario
 def menu():
     while True:
         print("\nAgenda de Pedidos")
@@ -62,9 +66,15 @@ def menu():
         else:
             print("Opción no válida. Por favor, elija una opción del menú.")
 
+def stock():
+    print("Productos disponibles: ")
+    print("1. Papas $2.999,99")
+    print("2. VelasX10 $1.999,99")
+    print("3. Vino $1.999,99")
+    print("4. Fernet 1L $9.999,99")
+    print("5. Bondiola x kg 6.200,00")
+    print("6. Fideos Tallarines 500g $2.300,00")
+    print("7. Copos de maiz azucarados 490g $3.535,00")
 
 
-
-# Ejecutamos el menú
-if __name__ == "__main__":
-    menu()
+menu()
