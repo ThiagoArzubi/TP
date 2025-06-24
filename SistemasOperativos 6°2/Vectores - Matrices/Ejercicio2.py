adfb79aafbff710199020cda0a780752ce1a8507
luci0qsy <arzubilobalbothiago@gmail.com>
2025-06-24 08:48:13 -0300

    papota

---
+++ SistemasOperativos 6°2/Vectores - Matrices/Ejercicio2.py
@@ -0,0 +1,28 @@
+import os
+os.system("cls")
+#Decoración: Nombre del Algoritmo
+print("--------------------------------")
+print(" Vector2: MEDIA DE TEMPERATURAS")
+print("--------------------------------")
+print("")
+
+Suma = 0
+Media = 0.0
+C = 0
+Temp = [] 
+
+print("Ingrese cantidad de Temperaturas: ")
+N = int( input())
+
+for i in range(N):
+    temperatura = float( input("Ingrese Temperatura {0}: ".format(i + 1) ))
+    Temp.append(temperatura)
+    Suma = Suma + Temp[i]
+Media = Suma / N 
+
+for tempElement in Temp: 
+    if tempElement >= Media:
+        C = C + 1
+        print(tempElement)
+print ("La media es ", Media)
+print ("Total de temperaturas >= a la media es", C)
---
+++ SistemasOperativos 6°2/Vectores - Matrices/Ejercicio3.py
@@ -0,0 +1,27 @@
+import os
+os.system("cls")
+#Decoración: Nombre del Algoritmo
+print("----------------------------------------------------------")
+print(" Vector3: ORDENAR Y QUITAR ELEMENTOS REPETIDOS DEL VECTOR")
+print("----------------------------------------------------------")
+print("")
+
+VEC = []
+print("Ingrese número de elementos del vector")
+N = int( input())
+
+if 1 <= N and N <= 200:
+    for i in range(1,N+1):
+        elemento = int( input("Ingrese Entero {0}: ".format(i)))
+    VEC.append(elemento)
+    i = 0
+    #TODO:
+    lista_nueva = [] 
+    for elemento in VEC:
+
+        if elemento not in lista_nueva:
+            lista_nueva.append(elemento)
+
+lista_nueva.sort()
+
+print(lista_nueva)
